0333a5b189aba194c982d7d2aa68716175fcd9bb
Mohamamd Mizanur Rahaman Onik <mizanonik@gmail.com>
2017-11-17 18:50:40 +0600

    modified the project and added a new function

--- practice/main.cpp
+++ practice/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+string fullName(string, string);
+
 int main()
 {
-    cout << "Welcome to github!" << endl;
+    string firstName;
+    string lastName;
+
+    cout << "Enter first name: "<< endl;
+    cin >> firstName;
+    cout << "Enter last name: "<< endl;
+    cin >> lastName;
+
+    cout << "Your full name is: " << fullName(firstName, lastName) << endl;
     return 0;
+}
+
+string fullName(string firstName, string lastName){
+    return firstName+ " " +lastName;
 }
