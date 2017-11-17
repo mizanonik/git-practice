0741a0215377ee61c1eacb3e4694ce63d4cb2941
Mohamamd Mizanur Rahaman Onik <mizanonik@gmail.com>
2017-11-17 21:26:54 +0600

    added a new method in the project

--- practice/main.cpp
+++ practice/main.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
-string fullName(string, string);
+string fullName(string, string);\
+string comment(int);
 
 int main()
 {
     string firstName;
     string lastName;
+    int age;
 
     cout << "Enter first name: "<< endl;
     cin >> firstName;
@@ -15,9 +17,22 @@ int main()
     cin >> lastName;
 
     cout << "Your full name is: " << fullName(firstName, lastName) << endl;
+
+    cout << "Enter your age: " << endl;
+    cin >> age;
+
+    cout << comment(age) << endl;
+
     return 0;
 }
 
 string fullName(string firstName, string lastName){
     return firstName+ " " +lastName;
+}
+
+string comment(int age){
+    if(age < 27){
+        return "You're a millennial";
+    }
+    return "You're getting old";
 }
